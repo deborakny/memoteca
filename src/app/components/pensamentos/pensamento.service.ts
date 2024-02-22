61344be5fc33ccfd4a4718ce139de1d432f181a0
Debora Kuniy <debora.n_@hotmail.com>
2024-02-22 19:16:33 -0300

    requisicao listar pensamentos

--- src/app/components/pensamentos/pensamento.service.ts
+++ src/app/components/pensamentos/pensamento.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
+  private readonly api = 'http://localhost:3000/pensamentos'
+
   constructor(
     private http: HttpClient
   ) { }
+
+  listarPensamentos(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.api);
+  }
 }
